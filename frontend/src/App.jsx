532762f7a077dd6be7095d389e78f4fe3a8f68f6
Vitor A Pantoja <vitorpantoja.dev@gmail.com>
2026-07-18 05:01:55 +0000

    Iniciado frontend com React+Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <header>
+      <h2>Bem vindo ao sistema de reserva de quadras esportivas da comunidade Atlântico</h2>
+    </header>
+  )
+}
+
+export default App
