b6dbb131f8d5647a0aa75b88efa8a963738fda5f
root <root@LAPTOP-NLA6555U.>
2026-07-20 13:08:48 -0300

    feat: cria o QuadraRepository com CRUD completo

---
+++ backend/src/repository/QuadraRepository.ts
@@ -0,0 +1,102 @@
+import meu_Prisma_Client_Configurado from "../database/PrismaClient.ts";
+import { PrismaClient } from "@prisma/client/extension";
+import type { quadra } from "../generated/prisma/client.ts"; 
+
+export class QuadraRepository {
+    
+    private prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = meu_Prisma_Client_Configurado;
+    }
+
+    
+    async insertQuadra(provided_nome: string, provided_modalidade: string, provided_localizacao: string)
+    : Promise<quadra | Error> {
+        try {
+            return await this.prisma.quadra.create({ 
+                data: {
+                    nome: provided_nome.trim(),
+                    modalidade: provided_modalidade.trim(),
+                    localizacao: provided_localizacao.trim(),
+                }
+            }); 
+        } catch (error: any) {
+            console.log(error);
+            return new Error(error); 
+        }
+    }
+
+    
+    async findAll(): Promise<quadra[] | Error> {
+        try {
+            return await this.prisma.quadra.findMany();
+        } catch (error: any) {
+            console.log(error);
+            return new Error(error);
+        }
+    }
+
+    async findById(provided_id: number): Promise<quadra | null | Error> {
+        try {
+            return await this.prisma.quadra.findUnique({
+                where: { id: provided_id },
+            });
+        } catch (error: any) {
+            console.log(error);
+            return new Error(error); 
+        }
+    }
+
+    async findByNome(provided_nome: string): Promise<quadra | null | Error> {
+        try {
+            return await this.prisma.quadra.findFirst({
+                where: { nome: provided_nome.trim() },
+            });
+        } catch (error: any) {
+            console.log(error);
+            return new Error(error);
+        }
+    }
+
+    async findByModalidade(provided_modalidade: string): Promise<quadra[] | Error> {
+        try {
+            return await this.prisma.quadra.findMany({
+                where: { modalidade: provided_modalidade.trim() },
+            });
+        } catch (error: any) {
+            console.log(error);
+            return new Error(error);
+        }
+    }
+    
+    
+    async updateQuadra(provided_id: number, provided_nome: string, provided_modalidade: string, provided_localizacao: string)
+    : Promise<quadra | Error> {
+        try {
+            return await this.prisma.quadra.update({
+                where: { id: provided_id },
+                data: {
+                    nome: provided_nome.trim(),
+                    modalidade: provided_modalidade.trim(),
+                    localizacao: provided_localizacao.trim()
+                }
+            });
+        } catch (error: any) {
+            console.log(error);
+            return new Error(error);
+        }
+    }
+
+    
+    async deleteQuadra(provided_id: number): Promise<quadra | Error> {
+        try {
+            return await this.prisma.quadra.delete({
+                where: { id: provided_id },
+            });
+        } catch (error: any) {
+            console.log(error);
+            return new Error(error);
+        }
+    }
+}
