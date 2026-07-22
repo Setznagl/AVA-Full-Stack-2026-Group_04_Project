6cc165f65eaa6df91a5136f6a17b321a8e40d2d7
Igor Ezequiel <igorezequielbarreto@gmail.com>
2026-07-22 17:42:58 -0300

    feat: adicionando quadraController e mapeamento de rotas

--- backend/routes.ts
+++ backend/routes.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import {unicJogadorController} from "./src/controller/JogadorController.ts";
+import { unicQuadraController } from "./src/controller/quadracontroller.ts";
 export const router = Router();
 /* Aqui estamos mapeando as rotas HTTP:
 Usando router.get ou app.get por exemplo ele tem como parâmetro de entrada uma string que é o path da URL
@@ -14,3 +15,11 @@ router.get("/jogador/:id", unicJogadorController.findByID.bind(unicJogadorContro
 router.get("/jogador", unicJogadorController.findByEmail.bind(unicJogadorController))
 router.put("/jogador", unicJogadorController.updateJogador.bind(unicJogadorController))
 router.delete("/jogador/:id", unicJogadorController.deleteJogador.bind(unicJogadorController))
+
+router.post("/quadra", unicQuadraController.insertQuadra.bind(unicQuadraController));
+router.get("/quadra/many", unicQuadraController.findAll.bind(unicQuadraController));
+router.get("/quadra/:id", unicQuadraController.findById.bind(unicQuadraController));
+router.get("/quadra/nome/:nome", unicQuadraController.findByNome.bind(unicQuadraController));
+router.get("/quadra/modalidade/:modalidade", unicQuadraController.findByModalidade.bind(unicQuadraController));
+router.put("/quadra", unicQuadraController.updateQuadra.bind(unicQuadraController));
+router.delete("/quadra/:id", unicQuadraController.deleteQuadra.bind(unicQuadraController));
