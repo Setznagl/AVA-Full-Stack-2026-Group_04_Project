912ea349b2278d54210851624b12519a8b4fee3c
Vitor A Pantoja <vitorpantoja.dev@gmail.com>
2026-07-31 09:53:35 -0300

    add cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import {router} from "./routes.ts";
 
 const port = process.env.PORT || 3000;
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(port, () => {  console.log("Server started on port", port);  });
