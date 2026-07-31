e584f5a24c1ff66c250c6fbc369fb0e40d90ceda
Gabriel Setznagl <devznagl@gmail.com>
2026-07-31 15:32:02 -0300

    feat(Entity): Quadra
    test:  Implementação de documentação importável da API via Swagger - OpenAPI 3.0
    
    Author: devznagl@gmail.com

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
-import {router} from "./routes.ts";
+import {router} from "./src/routes.ts";
+
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './src/swagger.ts';
 
 const port = process.env.PORT || 3000;
 export const app = express();
 
 app.use(express.json());
 app.use("/", router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.listen(port, () => {  console.log("Server started on port", port);  });
 
 //URL para testar se a API está rodando corretamente na porta 3000
