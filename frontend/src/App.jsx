ce8d7e7797226a03fc3e8c1589f59af85124fddc
Carlos Leonello <carlosleonello@hotmail.com>
2026-08-08 12:22:24 -0300

    feat: adiciona tela de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 import PlaceholderPage from "./pages/PlaceholderPage/PlaceholderPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<PlaceholderPage titulo="Login" />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/cadastro" element={<PlaceholderPage titulo="Criar Conta" />} />
       <Route path="/quadras" element={<PlaceholderPage titulo="Quadras" />} />
       <Route path="/reservar" element={<PlaceholderPage titulo="Reservar Horário" />} />
