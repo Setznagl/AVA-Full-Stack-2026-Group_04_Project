9b3ed938ba804d49f613659bc2553128882ca190
Vitor A Pantoja <vitorpantoja.dev@gmail.com>
2026-08-18 21:29:22 -0300

    fix import e chamada de useNavigate em LoginPage, ajusta gitignore

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import api from "../../services/Api";
 import "./LoginPage.css";
 
 function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
   const [message, setMessage] = useState("");
-  const navigate = usenavigate();
+  const navigate = useNsavigate();
 
   function decodeJwtPayload(token) {
     const payloadBase64 = token.split(".")[1];
